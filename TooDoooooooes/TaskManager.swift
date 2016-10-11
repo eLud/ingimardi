9cbf2163a4efe2cd35d937f89b90f4b261b9dae3
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-10-11 21:48:18 +0200

    Added a TaskManager class

---
+++ TooDoooooooes/TaskManager.swift
@@ -0,0 +1,42 @@
+//
+//  TaskManager.swift
+//  TooDoooooooes
+//
+//  Created by Ludovic Ollagnier on 11/10/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+import Foundation
+
+class TaskManager {
+
+    static let instance = TaskManager()
+
+    private var tasks: [Task]
+
+    var allTasks: [Task] {
+        return tasks
+    }
+
+    var tasksByPriority: [Task] {
+        let orderedTasks = tasks.sorted() { (t1, t2) -> Bool in
+            return t1.priority.rawValue < t2.priority.rawValue
+        }
+        return orderedTasks
+    }
+
+    var tasksByDate: [Task] {
+        let orderedTasks = tasks.sorted() { (t1, t2) -> Bool in
+            return t1.dueDate < t2.dueDate
+        }
+        return orderedTasks
+    }
+
+    private init() {
+        tasks = []
+    }
+
+    func add(_ task: Task) {
+        tasks.append(task)
+    }
+}
