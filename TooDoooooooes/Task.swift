4f5a319327c8dd39b3d2c0b07f552cde33057f66
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-10-11 21:18:07 +0200

    Added a Task model file

---
+++ TooDoooooooes/Task.swift
@@ -0,0 +1,31 @@
+//
+//  Task.swift
+//  TooDoooooooes
+//
+//  Created by Ludovic Ollagnier on 11/10/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+import Foundation
+
+struct Task {
+
+    enum Priority: Int {
+        case low = 0
+        case normal = 1
+        case high = 2
+    }
+
+    enum TaskType {
+        case personal
+        case professional
+    }
+
+    let name: String
+    let description: String
+
+    let dueDate: Date
+    var priority: Priority = Priority.normal
+    let type: TaskType
+    var imageURL: URL?
+}
