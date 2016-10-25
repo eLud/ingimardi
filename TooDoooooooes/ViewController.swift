e7a8cb3f1ee8f92f1522a0493203d6d66138638a
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-10-25 19:14:49 +0200

    Outlets and actions demo

--- TooDoooooooes/ViewController.swift
+++ TooDoooooooes/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonToHide: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var isPersoSwitch: UISwitch!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func save(_ sender: UIButton) {
 
+    }
+
+    @IBAction func toggleVisibility(_ sender: UIButton) {
+
+        UIView.animate(withDuration: 4) {
+        self.buttonToHide.isHidden = !self.buttonToHide.isHidden
+            sender.backgroundColor = UIColor.red
+        }
+    }
 }
 
